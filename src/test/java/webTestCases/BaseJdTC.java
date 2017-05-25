7c04cedea080df6c76550b3675da89e57c933273
zhl463 <zhl463@163.com>
2017-05-25 14:59:27 +0800

    Update BaseJdTC.java

--- src/test/java/webTestCases/BaseJdTC.java
+++ src/test/java/webTestCases/BaseJdTC.java
@@ -35,7 +35,7 @@ public class BaseJdTC {
         System.setProperty("selenide.browser","chrome");
         Configuration.browser="chrome";
 //        最大化窗口
-        startMaximized=true;
+        startMaximized=fasle;
 //        打开url地址
         open(baseUrl);
     }
